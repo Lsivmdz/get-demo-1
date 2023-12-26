27de1674ef22a05aa3b41bdd83fbefc423f50494
jeff <a13577@hotmail.com>
2023-12-26 21:43:57 +0800

    完成遊戲

--- game.py
+++ game.py
@@ -1,7 +1,18 @@
 import random 
 
 #1-50
+#可以猜五次
+#猜對提前離開
+#猜錯，提示答案
+#題目
 
-for i in range(10):
-    x=random.randint(1,50)
-    print(x)
+x=random.randint(1,50)
+print(x)
+for i in range(5):
+    y=int(input('請猜數字(1~50):'))    
+    if y==x:
+        print('猜對了!')
+        break
+    else:
+        print('猜錯了~~')
+
